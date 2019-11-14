ffae69d0bc01b13cc60257dea4664f25cd717f62
HarishSai05 <57615457+HarishSai05@users.noreply.github.com>
2019-11-14 20:35:08 +0530

    Source files added

---
+++ TravellerGuideWebApi/TravellerGuideWebApi/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace TravellerGuideWebApi.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ TravellerGuideWebApi/TravellerGuideWebApi/Areas/HelpPage/ModelDescriptions/IModelDocumentationProvider.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Reflection;
+
+namespace TravellerGuideWebApi.Areas.HelpPage.ModelDescriptions
+{
+    public interface IModelDocumentationProvider
+    {
+        string GetDocumentation(MemberInfo member);
+
+        string GetDocumentation(Type type);
+    }
+}
